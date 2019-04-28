bafe42d28a4500e54fd34bb2df2c877859094360
YenNhiTran <nhitty099@gmail.com>
2019-04-28 16:09:18 +0700

    final project

--- ProjectNhom/Demo/TEST_BMMANG/Form1.Designer.cs
+++ ProjectNhom/Demo/TEST_BMMANG/Form1.Designer.cs
@@ -35,14 +35,39 @@
             this.txt_message = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txttextdecode = new System.Windows.Forms.TextBox();
+            this.InputImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.OutputImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.fileAdressTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDecryption = new System.Windows.Forms.TextBox();
+            this.btnchouse = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputImagePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImagePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_openfile
             // 
-            this.btn_openfile.Location = new System.Drawing.Point(85, 257);
+            this.btn_openfile.Location = new System.Drawing.Point(358, 65);
             this.btn_openfile.Name = "btn_openfile";
-            this.btn_openfile.Size = new System.Drawing.Size(75, 23);
+            this.btn_openfile.Size = new System.Drawing.Size(75, 21);
             this.btn_openfile.TabIndex = 1;
             this.btn_openfile.Text = "Open File";
             this.btn_openfile.UseVisualStyleBackColor = true;
@@ -50,9 +75,9 @@
             // 
             // btn_Encode
             // 
-            this.btn_Encode.Location = new System.Drawing.Point(189, 351);
+            this.btn_Encode.Location = new System.Drawing.Point(358, 173);
             this.btn_Encode.Name = "btn_Encode";
-            this.btn_Encode.Size = new System.Drawing.Size(75, 23);
+            this.btn_Encode.Size = new System.Drawing.Size(75, 21);
             this.btn_Encode.TabIndex = 2;
             this.btn_Encode.Text = "Encode";
             this.btn_Encode.UseVisualStyleBackColor = true;
@@ -60,9 +85,9 @@
             // 
             // btn_Decode
             // 
-            this.btn_Decode.Location = new System.Drawing.Point(287, 351);
+            this.btn_Decode.Location = new System.Drawing.Point(358, 218);
             this.btn_Decode.Name = "btn_Decode";
-            this.btn_Decode.Size = new System.Drawing.Size(75, 23);
+            this.btn_Decode.Size = new System.Drawing.Size(75, 21);
             this.btn_Decode.TabIndex = 3;
             this.btn_Decode.Text = "Decode";
             this.btn_Decode.UseVisualStyleBackColor = true;
@@ -70,14 +95,14 @@
             // 
             // txt_FilePath
             // 
-            this.txt_FilePath.Location = new System.Drawing.Point(189, 259);
+            this.txt_FilePath.Location = new System.Drawing.Point(358, 92);
             this.txt_FilePath.Name = "txt_FilePath";
             this.txt_FilePath.Size = new System.Drawing.Size(173, 20);
             this.txt_FilePath.TabIndex = 4;
             // 
             // txt_message
             // 
-            this.txt_message.Location = new System.Drawing.Point(189, 304);
+            this.txt_message.Location = new System.Drawing.Point(358, 147);
             this.txt_message.Name = "txt_message";
             this.txt_message.Size = new System.Drawing.Size(173, 20);
             this.txt_message.TabIndex = 5;
@@ -85,38 +110,238 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(85, 304);
+            this.label1.Location = new System.Drawing.Point(355, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 14);
+            this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "Enter Message";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(44, 33);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(180, 151);
+            this.pictureBox1.Size = new System.Drawing.Size(349, 313);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(548, 351);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txttextdecode);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txt_message);
+            this.tabPage1.Controls.Add(this.btn_openfile);
+            this.tabPage1.Controls.Add(this.txt_FilePath);
+            this.tabPage1.Controls.Add(this.btn_Encode);
+            this.tabPage1.Controls.Add(this.btn_Decode);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(540, 325);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Giấu văn bản";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBoxOutput);
+            this.tabPage2.Controls.Add(this.textBoxDecryption);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.btnchouse);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.fileAdressTextBox);
+            this.tabPage2.Controls.Add(this.OutputImagePictureBox);
+            this.tabPage2.Controls.Add(this.InputImagePictureBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(540, 325);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Giấu ảnh";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txttextdecode
+            // 
+            this.txttextdecode.Location = new System.Drawing.Point(358, 245);
+            this.txttextdecode.Name = "txttextdecode";
+            this.txttextdecode.Size = new System.Drawing.Size(173, 20);
+            this.txttextdecode.TabIndex = 7;
+            // 
+            // InputImagePictureBox
+            // 
+            this.InputImagePictureBox.Location = new System.Drawing.Point(6, 56);
+            this.InputImagePictureBox.Name = "InputImagePictureBox";
+            this.InputImagePictureBox.Size = new System.Drawing.Size(231, 171);
+            this.InputImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.InputImagePictureBox.TabIndex = 0;
+            this.InputImagePictureBox.TabStop = false;
+            // 
+            // OutputImagePictureBox
+            // 
+            this.OutputImagePictureBox.Location = new System.Drawing.Point(282, 56);
+            this.OutputImagePictureBox.Name = "OutputImagePictureBox";
+            this.OutputImagePictureBox.Size = new System.Drawing.Size(245, 171);
+            this.OutputImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OutputImagePictureBox.TabIndex = 0;
+            this.OutputImagePictureBox.TabStop = false;
+            // 
+            // fileAdressTextBox
+            // 
+            this.fileAdressTextBox.Location = new System.Drawing.Point(103, 7);
+            this.fileAdressTextBox.Name = "fileAdressTextBox";
+            this.fileAdressTextBox.Size = new System.Drawing.Size(207, 20);
+            this.fileAdressTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ảnh cần giấu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Chọn ảnh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ảnh nền";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Chọn ảnh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(282, 233);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(245, 10);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(87, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "encrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Chọn ảnh mã hóa";
+            // 
+            // textBoxDecryption
+            // 
+            this.textBoxDecryption.Location = new System.Drawing.Point(103, 256);
+            this.textBoxDecryption.Name = "textBoxDecryption";
+            this.textBoxDecryption.Size = new System.Drawing.Size(207, 20);
+            this.textBoxDecryption.TabIndex = 9;
+            // 
+            // btnchouse
+            // 
+            this.btnchouse.Location = new System.Drawing.Point(331, 258);
+            this.btnchouse.Name = "btnchouse";
+            this.btnchouse.Size = new System.Drawing.Size(75, 23);
+            this.btnchouse.TabIndex = 5;
+            this.btnchouse.Text = "Chọn ảnh";
+            this.btnchouse.UseVisualStyleBackColor = true;
+            this.btnchouse.Click += new System.EventHandler(this.btnchouse_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Lưu tại";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(103, 288);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(207, 20);
+            this.textBoxOutput.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(331, 285);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Decrypt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txt_message);
-            this.Controls.Add(this.txt_FilePath);
-            this.Controls.Add(this.btn_Decode);
-            this.Controls.Add(this.btn_Encode);
-            this.Controls.Add(this.btn_openfile);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(545, 356);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputImagePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImagePictureBox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -128,6 +353,26 @@
         private System.Windows.Forms.TextBox txt_message;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txttextdecode;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fileAdressTextBox;
+        private System.Windows.Forms.PictureBox OutputImagePictureBox;
+        private System.Windows.Forms.PictureBox InputImagePictureBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxDecryption;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnchouse;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ProjectNhom/Demo/TEST_BMMANG/Form1.cs
+++ ProjectNhom/Demo/TEST_BMMANG/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,12 @@ namespace TEST_BMMANG
             InitializeComponent();
         }
 
+        OpenFileDialog ofdImage;
+        OpenFileDialog ofdFile;
+        Bitmap bmp;
+        const int sizeAlloc = 512;
+        OpenFileDialog decryptOfd;
+        string openpath = "";
         private void btn_openfile_Click(object sender, EventArgs e)
         {
             OpenFileDialog openDialog = new OpenFileDialog();
@@ -95,7 +102,176 @@ namespace TEST_BMMANG
                     }
                 }
             }
-            txt_message.Text = message;
+            txttextdecode.Text = message;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofdFile = new OpenFileDialog();
+            ofdFile.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                fileAdressTextBox.Text = ofdFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofdImage = new OpenFileDialog();
+            ofdImage.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            if (ofdImage.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(ofdImage.FileName);
+                InputImagePictureBox.Image = bmp;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            FileStream fs = new FileStream(ofdFile.FileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            byte[] bArray = br.ReadBytes((int)fs.Length);
+            int size = bArray.Length;
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+
+
+            int k = 0;
+
+
+            int tempSize = size;
+
+            for (int i = 0; i < sizeAlloc; i++)
+            {
+                byte A = 0, R = 0, G = 0, B = 0;
+                byte[] ar = new byte[4] { A, R, G, B };
+                for (int h = 0; h < 4; h++)
+                {
+                    if (tempSize >= 255)
+                    {
+                        ar[h] = 255;
+                        tempSize -= 255;
+
+                    }
+                    else if (tempSize < 255)
+                    {
+                        ar[h] = (byte)tempSize;
+                        tempSize = 0;
+                    }
+
+                }
+                bmp2.SetPixel(i, 0, Color.FromArgb(ar[0], ar[1], ar[2], ar[3])); //TODO : Can allow multi-line length specification
+            }
+            int count = 0;
+
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+
+                    byte G = bmp.GetPixel(i, j).G;
+                    byte B = bmp.GetPixel(i, j).B;
+
+                    if (k < bArray.Length && count > sizeAlloc)
+                    {
+                        bmp2.SetPixel(i, j, Color.FromArgb(255, bArray[k], G, B));
+                        k++;
+                    }
+                    else
+                    {
+                        byte R = bmp.GetPixel(i, j).R;
+                        bmp2.SetPixel(i, j, Color.FromArgb(255, R, G, B));
+                    }
+                    count++;
+
+                }
+                backgroundWorker1.ReportProgress((int)(((double)(i + 1) / (double)bmp.Width) * 100d));
+            }
+
+            br.Close();
+            fs.Close();
+            e.Result = bmp2;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void btnchouse_Click(object sender, EventArgs e)
+        {
+            decryptOfd = new OpenFileDialog();
+            if (decryptOfd.ShowDialog() == DialogResult.OK)
+            {
+                openpath = decryptOfd.FileName;
+                textBoxDecryption.Text = openpath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp3;
+
+            if (openpath != "")
+            {
+                bmp3 = new Bitmap(openpath);
+            }
+            else
+            {
+                MessageBox.Show("Not put a path");
+                return;
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Image Files(*.PNG)|*.PNG;|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxOutput.Text = sfd.FileName;
+                FileStream rf = new FileStream(sfd.FileName, FileMode.Create);
+                BinaryWriter wr = new BinaryWriter(rf);
+
+                int genSize = 0;
+
+                for (int i = 0; i < sizeAlloc; i++)
+                {
+                    genSize += bmp3.GetPixel(i, 0).A + bmp3.GetPixel(i, 0).R + bmp3.GetPixel(i, 0).G + bmp3.GetPixel(i, 0).B;
+                }
+
+                int k = 0;
+                int count = 0;
+                for (int i = 0; i < bmp3.Width; i++)
+                {
+                    for (int j = 0; j < bmp3.Height; j++)
+                    {
+                        if (k < genSize && count > sizeAlloc)
+                        {
+                            byte val = bmp3.GetPixel(i, j).R;
+                            wr.Write(val);
+                            k++;
+                        }
+                        count++;
+                    }
+                }
+                wr.Close();
+                rf.Close();
+            }
+            MessageBox.Show("Image saved at : " + sfd.FileName);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            OutputImagePictureBox.Image = (Bitmap)e.Result;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Image Files(*.PNG)|*.PNG;|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                OutputImagePictureBox.Image.Save(sfd.FileName);
+            }
         }
     }
 }
